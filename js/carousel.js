b49c89192ab98236a87f97e6d4fb3bdac22b1e41
Andrey Dorodnev <andrew992@mail.ru>
2020-04-19 13:09:26 +0300

    initial

---
+++ js/carousel.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".slider1").owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    dots:true,
+    navText:[],
+    responsive:{
+        0:{
+            items:1,
+            loop:true,
+            dots:true,
+            nav:false
+        }
+
+    }
+});
+});
