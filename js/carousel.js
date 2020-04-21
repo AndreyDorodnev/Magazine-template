d99dfe42bef4b725b7599a4c842b8ead76c67e04
Andrey Dorodnev <andrew992@mail.ru>
2020-04-21 14:05:58 +0300

    blogs carousel add

--- js/carousel.js
+++ js/carousel.js
@@ -1,37 +1,69 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".slider1").owlCarousel({
-    // loop:true,
-    margin:0,
-    autoWidth:true,
-    nav:true,
-    dots:true,
-    navText:[],
-    items:5,
-    responsive:{
-        0:{
-            items:1,
-            // loop:true,
-            dots:true,
-            nav:false
-        },
-        500:{
-            items:2,
-            loop:true,
-            dots:true,
-            nav:false
-        },
-        840:{
-            items:4,
-            loop:true,
-            dots:true,
-            nav:false
-        },
-        1490:{
-            items:5,
-            loop:true,
-            dots:false,
-            nav:false 
+        // loop:true,
+        margin: 0,
+        autoWidth: true,
+        nav: true,
+        dots: true,
+        navText: [],
+        items: 5,
+        responsive: {
+            0: {
+                items: 1,
+                // loop:true,
+                dots: true,
+                nav: false
+            },
+            500: {
+                items: 2,
+                loop: true,
+                dots: true,
+                nav: false
+            },
+            840: {
+                items: 4,
+                loop: true,
+                dots: true,
+                nav: false
+            },
+            1490: {
+                items: 5,
+                loop: true,
+                dots: false,
+                nav: false
+            }
         }
-    }
+    });
 });
+
+$(document).ready(function () {
+    $(".slider2").owlCarousel({
+        // loop:true,
+        margin: 0,
+        autoWidth: true,
+        nav: true,
+        dots: true,
+        navText: [],
+        items: 4,
+        responsive: {
+            0: {
+                items: 1,
+                // loop:true,
+                dots: true,
+                nav: false
+            },
+            500: {
+                items: 2,
+                loop: true,
+                dots: true,
+                nav: false
+            },
+            840: {
+                items: 4,
+                loop: false,
+                dots: true,
+                nav: false
+            }
+        }
+    });
 });
